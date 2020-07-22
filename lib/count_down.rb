63075c5d1e02283b523d9c3db3bd6df2f1398760
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-07-22 20:39:37 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,9 +1,9 @@
 # Write your code here
 
 def count_down
-count = 10
-until count = 0 do
-  puts "#{count}"
-end
-puts "Happy New Year"
+  count = 10
+  until count = 0 do
+    puts "#{count}"
+  end
+  puts "Happy New Year"
 end
